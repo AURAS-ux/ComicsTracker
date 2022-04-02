ab1ae0f333440958ed5a77ec3ec7fa7203781bbd
Auras Paltanea <auraspaltaneamarian@gmail.com>
2022-04-02 15:33:10 +0300

    fixes
    fixed ui

---
+++ ComicsTracker/Comic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComicsTracker
+{
+    public class Comic
+    {
+        public String Name { get; set; }   
+        public bool Finished { get; set; }
+        public Comic(String name,bool finished)
+        {
+            this.Name = name;
+            this.Finished = finished;
+        }
+    }
+}
--- ComicsTracker/MainWindow.xaml.cs
+++ ComicsTracker/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Xml;
 
 namespace ComicsTracker
 {
@@ -20,9 +12,61 @@ namespace ComicsTracker
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<Comic> comics { get; set; }
+
+
         public MainWindow()
-        {
+        {   
             InitializeComponent();
+
+
+            comics = new List<Comic>();
+            comics = LoadComics();
+            this.DataContext = this;
+            Mylist.ItemsSource = comics;
+
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Border_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+
+        public List<Comic> LoadComics()
+        {
+            List<Comic> coms = new List<Comic>();
+
+            Comic com1 = new Comic("Batman", true);
+            Comic com2 = new Comic("Flash", true);
+            Comic com3 = new Comic("Booster", false);
+            coms.Add(com1);
+            coms.Add(com2);
+            coms.Add(com3);
+            XmlTextWriter xmlTextWriter = new XmlTextWriter("D:/cs/ComicsTracker/ComicsTracker/data.xml", System.Text.Encoding.UTF8);
+            xmlTextWriter.Formatting = Formatting.Indented;
+            xmlTextWriter.WriteStartDocument();
+            xmlTextWriter.WriteStartElement("comics");
+            for(int i = 0; i < coms.Count; i++)
+            {
+                xmlTextWriter.WriteElementString("name",coms[i].Name);
+                xmlTextWriter.WriteElementString("status", coms[i].Finished.ToString());
+            }
+            xmlTextWriter.WriteEndElement();
+            xmlTextWriter.WriteEndDocument();
+            xmlTextWriter.Flush();
+            xmlTextWriter.Close();
+            return coms;
         }
     }
 }
